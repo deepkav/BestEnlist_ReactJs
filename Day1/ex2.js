7348ae8c822c7c669bf3c95faa753ed27fe6aec5
DeepakNithin <9918004210@klu.ac.in>
2020-12-02 23:28:18 +0530

    ex2

---
+++ Day1/ex2.js
@@ -0,0 +1,12 @@
+// Write a JavaScript program to check two given numbers and return true if one of the number is 50 or if their sum is 50.
+
+var a = Number(prompt());
+var b = Number(prompt());
+
+console.log(a == 50 || b == 50 || a + b == 50 ? true : false);
+
+/* OutPut:
+a = 20
+b = 30
+true
+*/
