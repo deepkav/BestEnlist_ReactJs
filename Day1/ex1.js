f675c13781b48235b705b803138feb6d7ae0e77a
DeepakNithin <9918004210@klu.ac.in>
2020-12-02 23:00:31 +0530

    Create ex1.js

---
+++ Day1/ex1.js
@@ -0,0 +1,7 @@
+//	Write a JavaScript program to calculate days left until next Christmas 
+var xmas = new Date("25 December 2020");
+var current_Day = new Date();
+var difference_In_Time = xmas.getTime() - current_Day.getTime();
+var difference_In_Days = difference_In_Time / (1000 * 3600 * 24);
+console.log(difference_In_Days);
+//output: 22.052312210648147
